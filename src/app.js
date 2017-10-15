f8d2aa9fc99f52dc4700bb4b015f9aaa4fb3ac5e
kobra8 <jerzydrobot@gmail.com>
2017-10-15 22:48:20 +0200

    Basic structure

---
+++ src/app.js
@@ -0,0 +1,29 @@
+var vueApp = new Vue({
+    el: '#app',
+    data: {
+        invite:"Welcome to VueJS",
+        name:"",
+        message:"Taki tooltip malutki :)",
+        seen: true,
+        cars: [
+          {car: 'Honda'},
+          {car: 'Toyota'},
+          {car: 'Suzuki'},
+          {car: 'Mazda'}
+        ],
+        days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        buttonName: 'Hide text'
+    },
+    methods: {
+        hide: function() {
+            if(this.seen === true) {
+            this.seen = false;
+            this.buttonName='Show text';
+            }
+            else {
+                this.seen = true;
+                this.buttonName='Hide text';
+            }
+        }
+    }
+})
