b30ecdc7201d66372b2395bd8d21e204089da7a0
kobra8 <jerzydrobot@gmail.com>
2018-01-02 16:25:31 +0100

    Exercise 4 styling

--- exercises/Exercise 4/app.js
+++ exercises/Exercise 4/app.js
@@ -1,11 +1,34 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effectClass: '',
+    customClasses: [
+      'shadow', 'header', 'container'
+    ],
+    inputClass: '',
+    inputClasses: [''],
+    userClass:'',
+    userBoolean: false,
+    color: 'white',
+    barWidth: 0
   },
   methods: {
-    startEffect: function() {
-    
+    startEffect: function () {
+      this.effectClass = "highlight";
+      setTimeout(() => {
+        this.effectClass = "shrink";
+      }, 2000)
+    },
+    attachClass: function () {
+      this.inputClasses.push(this.inputClass);
+    },
+    startProgress: function () {
+     let interval = setInterval(()=> {
+        this.barWidth = this.barWidth + 2;
+        if (this.barWidth > 500) {
+          clearInterval(interval)
+        }
+      }, 500);
     }
   }
 });
